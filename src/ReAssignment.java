3fb493295e01c507c4b13ba0c925bacc263e3936
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-03 20:47:25 -0500

    Finished

--- src/ReAssignment.java
+++ src/ReAssignment.java
@@ -6,9 +6,9 @@
 
 /*
  	Your predicted values for variables a, b, and c
-	a => 
-	b =>
-	c => 
+	a => 5 + 1 = 6 correct
+	b => 10 - 1 = 9 correct
+	c =>  9 + 5 = 14 incorrect -   int c = int b = 10 +  variable a = 6 = 10 + 6 = 16
  
  	After testing the statements in the main method, indicate above if your predictions were correct.
 */
@@ -16,6 +16,17 @@
 public class ReAssignment {
 
 	public static void main(String[] args) {
+		int a = 5; 
+		int b = 10; 
+		int c = b;
+		a = a + 1; 
+		b = b - 1; 
+		c = c + a;
+		b = c % a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	
 		// TODO Auto-generated method stub
 
 	}
