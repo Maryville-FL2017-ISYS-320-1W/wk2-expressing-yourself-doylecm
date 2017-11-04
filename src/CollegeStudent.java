2816b5d9d2e68b4bda26b2c66fda832a29a74a96
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-03 21:33:10 -0500

    Finished

--- src/CollegeStudent.java
+++ src/CollegeStudent.java
@@ -1,13 +1,20 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Chris Doyle
+  	Date: 11/3/2017
 */
 
 public class CollegeStudent {
-
 	public static void main(String[] args) {
+		String a = "Junior";
+		int b = 2;
+		double c = 3.8;
+		
+		System.out.println("Chris Doyle");
+		System.out.println("Academic Year:" + a);
+		System.out.println("Number of Classes Being Taken:" + b);
+		System.out.println("Grade Point Average:" + c);
+		
 
 	}
-
 }
