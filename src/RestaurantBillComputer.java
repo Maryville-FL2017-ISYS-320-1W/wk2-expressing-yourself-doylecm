033b6ed897dacabe656a90ef169748b0004a62f8
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-04 12:48:39 -0500

    Revised

--- src/RestaurantBillComputer.java
+++ src/RestaurantBillComputer.java
@@ -1,29 +1,42 @@
 
 public class RestaurantBillComputer {
 	public static void main(String[] args) {
-		
-		double a = 20.00; // Bill total
-		double b = .15; // 15% gratuity
-		double c = .18; // 18% gratuity
-		double d = .20; //20% gratuity
-		
+		Gratuity1();
+		Gratuity2();
+		Gratuity3();
+	}
+	
+	public static void line1() {
 		System.out.println("Subtotal:$" + a);
+	}
+	
+	public static void line2() {
+		System.out.println();
+	}
+		
+		static double a = 25.00; // Bill total
+		static double b = .15; // 15% gratuity
+		static double c = .18; // 18% gratuity
+		static double d = .20; //20% gratuity
+		
+		public static void Gratuity1() {
+		line1();
 		System.out.println("Gratuity:15%");
 		System.out.println("   Total:$" + ((a * b) + a));
-		System.out.println();
+		line2();
+		}
 		
-		System.out.println("Subtotal:$" + a);
+		public static void Gratuity2( ) {
+		line1();
 		System.out.println("Gratuity:18%");
 		System.out.println("   Total:$" + ((a * c) + a));
-		System.out.println();
+		line2();
+		}
 		
-		System.out.println("Subtotal:$" + a);
+		public static void Gratuity3() {
+		line1();
 		System.out.println("Gratuity:20%");
 		System.out.println("   Total:$" + ((a * d) + a));
-	}
-
-	private static void line1() {
-		// TODO Auto-generated method stub
+		}
 		
-	}
 }
