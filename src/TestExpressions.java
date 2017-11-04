8227f5ba589a88737bff5278823deb370499a4ed
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-03 19:29:15 -0500

    Finished

--- src/TestExpressions.java
+++ src/TestExpressions.java
@@ -6,14 +6,14 @@
 
 /*
  	Your predicted values for the expressions
-	1.
-	2.
-	3.
-	4.
-	5.
-	6.
-	7.
-	8.
+	1. 8 correct
+	2. 7 incorrect - multiply and divide hold the same level of precedence, as opposed to Order of Operations
+	3. 3 incorrect - same reason as above
+	4. 4 correct
+	5. 2 correct
+	6. 11 correct
+	7. "2 + 2" + 7 incorrect - writes 2 + 2, followed by 3, then 4.
+	8. 7 + " 2 + 2" incorrect - adds 3 + 4, then writes sum, followed by 2 + 2
  
  	After testing the expressions in the main method, indicate above if your predictions were correct.
 */
@@ -21,7 +21,7 @@
 public class TestExpressions {
 
 	public static void main(String[] args) {
-		System.out.println("YOUR_TEST_EXPRESSION");
+		System.out.println("2 + 2" + 3 + 4);
 	}
 
 }
