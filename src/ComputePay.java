7f353962f36d0c37eae5c44ff3c254705637b45a
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-03 20:52:45 -0500

    Finished

--- src/ComputePay.java
+++ src/ComputePay.java
@@ -1,18 +1,20 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Chris Doyle
+  	Date: 11/3/2017
 */
 
 public class ComputePay {
 	public static void main(String[] args) {
+		int a = 4 + 5 + 8 + 4; // hours worked
+		double b = 8.75; // hourly pay
 		System.out.println("My total hours worked:");
-		System.out.println(4 + 5 + 8 + 4);
+		System.out.println(a);
 		
 		System.out.println("My hourly salary:");
-		System.out.println("$8.75");
+		System.out.println("$" + b);
 		
 		System.out.println("My total pay:");
-		System.out.println((4 + 5 + 8 + 4) * 8.75);
+		System.out.println("$" + a * b);
 	}
 }
